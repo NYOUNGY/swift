111c0fbcfe58bbc0edbd6ca42ff41748aefd524b
nyoungy <nyoung3616@naver.com>
2022-06-11 09:28:55 +0900

    22/06/11 first commit

---
+++ KBJuniorPO/KBJuniorPO/KBJuniorPOApp.swift
@@ -0,0 +1,17 @@
+//
+//  KBJuniorPOApp.swift
+//  KBJuniorPO
+//
+//  Created by nyoungy on 2022/06/11.
+//
+
+import SwiftUI
+
+@main
+struct KBJuniorPOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var greeting = "Hello, playground"
+greeting = "HEllO ~~~~~"
+
+print(greeting)
+
+
+let isLogin = false
+isLogin = true
+
+
