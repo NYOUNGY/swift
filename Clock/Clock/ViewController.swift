d4923dc29f357204be64c71f1d9b28c6724659f4
nyoungy <nyoung3616@naver.com>
2022-06-18 13:15:17 +0900

    22/06/18 fourth commit

--- Clock/Clock/ViewController.swift
+++ Clock/Clock/ViewController.swift
@@ -8,18 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var datePicker: UIDatePicker!
     @IBOutlet var clock: UITextField!
+    @IBOutlet var myDate: UITextField!
+    var nowtime: String = "a"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //1초마다 liveClock 호출
         Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(liveClock), userInfo: nil, repeats: true)
+        
     }
 
+
+    
     @objc func liveClock(){
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        let formatter2 = DateFormatter()
+        formatter2.dateFormat = "E MMM d HH:mm a"
+        
+        //상단 시계
         clock.text = formatter.string(from: Date())
+        
+        //날짜 비교용
+        nowtime = formatter2.string(from: Date())
+        
+        if(myDate.text == nowtime){
+            myDate.backgroundColor = UIColor.red
+            myDate.textColor = UIColor.white
+        }
     }
+    
+    @IBAction func datePicking(_ sender: UIDatePicker) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "E MMM d HH:mm a"
+        myDate.text = formatter.string(from: sender.date)
+    }
+    
 }
 
