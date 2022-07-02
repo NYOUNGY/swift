44bda66c30e4ed35e914955bffe5236026e79422
nyoungy <nyoung3616@naver.com>
2022-07-02 14:42:06 +0900

    third commit 22/07/02

---
+++ Assignment/Assignment/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  Assignment
+//
+//  Created by nyoungy on 2022/07/02.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
